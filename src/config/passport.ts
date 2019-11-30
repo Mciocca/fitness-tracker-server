1c220808686988ca280dd916906101094fa2bd77
Michael Ciocca <mciocca89@gmail.com>
2019-11-30 16:12:07 -0500

    create passport strategy for JWT authentication

---
+++ src/config/passport.ts
@@ -0,0 +1,59 @@
+import { Request } from 'express';
+import passport from 'passport';
+import passportJWT from 'passport-jwt';
+import passportLocal from 'passport-local';
+import User from '../entity/User';
+
+const LocalStrategy = passportLocal.Strategy;
+const JWTStrategy = passportJWT.Strategy;
+const JWT_COOKIE = 'jwt';
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// setting jwt happens in AuthController#authenticate since we don't have access to response here
+passport.use('login',
+  new LocalStrategy({
+    passwordField: 'password',
+    usernameField: 'email',
+  }, async (email: string, password: string, done) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+     return done(null, false, { message: 'Failed to login' });
+    }
+
+    if (await user.validLogin(password)) {
+      done(null, user, { message: 'Logged in'});
+    } else {
+      done(null, false, { message: 'Failed to login' });
+    }
+  }),
+);
+
+passport.use(new JWTStrategy(
+  {
+    jwtFromRequest: (req): string => {
+      let token: string;
+      if (req && req) {
+        token = req.cookies[JWT_COOKIE];
+      }
+      return token;
+    },
+    secretOrKey: JWT_SECRET,
+  },
+  async (jwtPayload, done) => {
+    let user: User;
+    try {
+      user = await User.findOne({ id: jwtPayload.userId });
+    } catch (e) {
+      done('You must login to view this page');
+    }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false, { message: 'You must login to view this page.'} );
+    }
+  }),
+);
+
+export default passport;
