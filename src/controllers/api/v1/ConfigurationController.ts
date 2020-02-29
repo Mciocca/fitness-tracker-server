effd00784526761d3184dcb19c91cf7d500b0a96
Michael Ciocca <mciocca89@gmail.com>
2020-02-29 16:28:26 -0500

    send crsf token down in configuration request

--- src/controllers/api/v1/ConfigurationController.ts
+++ src/controllers/api/v1/ConfigurationController.ts
@@ -3,6 +3,7 @@ import { links } from '../../../serializers/Configuration';
 
 export default class ConfigurationController {
   public static show(request: Request, res: Response): void {
+    res.cookie('XSRF-TOKEN', request.csrfToken());
     res.json({ links });
   }
 }
