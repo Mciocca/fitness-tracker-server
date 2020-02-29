6afeccbc7539e1250104a192c21032fd84b63aab
Michael Ciocca <mciocca89@gmail.com>
2020-02-29 16:28:06 -0500

    remove csurf from api controller

--- src/routes/apiV1.ts
+++ src/routes/apiV1.ts
@@ -1,14 +1,13 @@
-import csurf from 'csurf';
 import { Router } from 'express';
 import passport from '../config/passport';
 import ConfigurationController from '../controllers/api/v1/ConfigurationController';
 import UserController from '../controllers/api/v1/UserController';
 const router = Router();
 
-router.use(csurf({ cookie: true }));
 router.get('/api/v1', ConfigurationController.show);
 
 router.use(passport.authenticate('jwt', { session: false }));
 router.get('/api/v1/user', UserController.show);
+router.post('/api/v1/user', UserController.update);
 
 export default router;
