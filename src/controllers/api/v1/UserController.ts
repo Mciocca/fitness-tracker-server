20a80abbf8ef530911bf021477df548b1c41a54a
Michael Ciocca <mciocca89@gmail.com>
2020-03-02 20:22:54 -0500

    Add update method to user controller

--- src/controllers/api/v1/UserController.ts
+++ src/controllers/api/v1/UserController.ts
@@ -1,12 +1,22 @@
 import { Response } from 'express';
 import { serializeUser } from '../../../serializers/User';
-
-// TODO: figure out why passport req.user isn't on the req types.
+import UserUpdater from '../../../services/UserUpdater';
 export default class UserController {
   public static show(req: any, res: Response): void {
     const user = serializeUser(req.user);
 
     res.status(200);
     res.json({ user });
+  }
+
+  public static async update(req: any, res: Response) {
+    const updater = new UserUpdater(req.body.user, req.user);
+    if (await updater.update()) {
+      res.status(200);
+      res.json(serializeUser(updater.user));
+    } else {
+      res.status(updater.errorStatus);
+      res.json(updater.errors);
+    }
   }
 }
