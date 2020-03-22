ba88a68b1e65dd66bd634c5b0e656805eabd1a95
Michael Ciocca <mciocca89@gmail.com>
2020-03-21 20:45:31 -0400

    Fix issue in user serializer - profile is a promise that needs to be awaited

--- src/serializers/User.ts
+++ src/serializers/User.ts
@@ -2,7 +2,8 @@ import User from '../entity/User';
 import { enumValuesToArray } from '../utils/enumHelpers';
 import { Gender, Goals } from '../entity/Profile';
 
-export const serializeUser = (user: User) => {
+export const serializeUser = async (user: User) => {
+  const profile = await user.profile;
   return {
     email: user.email,
     firstName: user.firstName,
@@ -10,10 +11,10 @@ export const serializeUser = (user: User) => {
     lastName: user.lastName,
     name: user.fullName,
     profile: {
-      age: user.profile?.age,
-      gender: user.profile?.gender,
-      goal: user.profile?.goal,
-      height: user.profile?.height,
+      age: profile?.age,
+      gender: profile?.gender,
+      goal: profile?.goal,
+      height: profile?.height,
       options: {
         gender: enumValuesToArray(Gender),
         goals: enumValuesToArray(Goals)
