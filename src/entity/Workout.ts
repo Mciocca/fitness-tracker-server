ba0d859407b9af30fd018d469197ce437aa31c97
Michael Ciocca <mciocca89@gmail.com>
2020-04-21 21:51:34 -0400

    prepare Workout and WorkoutSet  for creation

--- src/entity/Workout.ts
+++ src/entity/Workout.ts
@@ -8,32 +8,41 @@ import {
   UpdateDateColumn,
   ManyToOne,
   OneToMany,
+  JoinColumn,
 } from 'typeorm';
 import User from './User';
 import WorkoutSet from './WorkoutSet';
+import { IsDefined, IsString, IsNumber } from 'class-validator';
 
 @Entity()
 export default class Workout extends BaseEntity {
   @ManyToOne(type => User, user => user.workouts)
+  @JoinColumn()
   public user: User;
 
   @OneToMany(type => WorkoutSet, set => set.workout, { onDelete: 'CASCADE', eager: true })
+  @JoinColumn()
   public sets: WorkoutSet[];
 
   @PrimaryGeneratedColumn()
   public id: number;
 
   @Column()
+  @IsDefined()
+  @IsString()
   public title: string;
 
   @Column({ type: 'text', nullable: true })
+  @IsString()
   public notes: string;
 
   @Column({ nullable: true })
+  @IsNumber()
   public weight: number;
 
   @Column({ type: 'date' })
-  public date: string;
+  @IsDefined({ message: 'Date is required'})
+  public date: Date;
 
   @CreateDateColumn()
   public createdAt: Timestamp;
--- src/entity/WorkoutSet.ts
+++ src/entity/WorkoutSet.ts
@@ -7,25 +7,40 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   ManyToOne,
+  JoinColumn,
 } from 'typeorm';
 import Workout from './Workout';
 import Exercise from './Exercise';
 
 @Entity()
 export default class WorkoutSet extends BaseEntity {
+  public static insertMany(records: []) {
+    this.createQueryBuilder()
+        .insert()
+        .into('workout_set')
+        .values(records)
+        .execute();
+  }
+
+  @Column()
+  public workoutId: number;
   @ManyToOne(type => Workout, workout => workout.sets)
+  @JoinColumn({ name:  'workoutId' })
   public workout: Workout;
 
+  @Column()
+  public exerciseId: number;
   @ManyToOne(type => Exercise, exercise => exercise.sets, { eager: true })
+  @JoinColumn({ name: 'exerciseId' })
   public exercise: Exercise;
 
   @PrimaryGeneratedColumn()
   public id: number;
 
-  @Column()
+  @Column({ default: 0 })
   public reps: number;
 
-  @Column()
+  @Column({ default: 0})
   public weight: number;
 
   @CreateDateColumn()
